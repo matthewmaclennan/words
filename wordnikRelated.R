ff36d32a9514372bbf82f7d7ab1896d9cc25a3bd
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-10-28 15:46:05 -0700

    Create wordnikRelated.R

---
+++ wordnikRelated.R
@@ -0,0 +1,2 @@
+#searches for words related to the term "vivacious".
+http://api.wordnik.com/v4/word.json/vivacious/relatedWords?useCanonical=false&relationshipTypes=synonym&limitPerRelationshipType=100&api_key=
